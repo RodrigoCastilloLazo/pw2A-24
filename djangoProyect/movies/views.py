b3693d992fae3a1723318bc54f189268f32f0e60
RodrigoCLz <rcastillolaz@gmail.com>
2024-05-26 18:08:18 -0500

    Se definio las vistas y url

--- djangoProyect/movies/views.py
+++ djangoProyect/movies/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Movie
+import json
 
-# Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = [{"id": movie.id, "title": movie.title, "year": movie.year, "genre": movie.genre} for movie in movies]
+    return JsonResponse(data, safe=False)
+
+@csrf_exempt
+def add_movie(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        movie = Movie(title=data['title'], year=data['year'], genre=data['genre'])
+        movie.save()
+        return JsonResponse({"id": movie.id}, status=201)
+    return JsonResponse({"error": "Invalid method"}, status=400)
