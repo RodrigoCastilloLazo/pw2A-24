9b5d62fd61985688f8c0de75da7cf803f5be9d64
RodrigoCLz <rcastillolaz@gmail.com>
2024-05-26 19:16:06 -0500

    urls.py terminada

--- djangoProyect/djangoProyect/urls.py
+++ djangoProyect/djangoProyect/urls.py
@@ -5,5 +5,4 @@ from movies.views import movie_list
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
-    path('', movie_list, name='movie_list'),  # Mapea la URL raíz a la vista de lista de películas
-]
+    path('', movie_list, name='movie_list'),  
