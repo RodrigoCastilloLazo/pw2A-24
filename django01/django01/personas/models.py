16e91104f55ea349897fdd5c70b5854bc6f9bb95
RodrigoCLz <rcastillolaz@gmail.com>
2024-06-01 12:02:46 -0500

    Se creo la clase Persona

--- django01/django01/personas/models.py
+++ django01/django01/personas/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
 # Create your models here.
-python manage.py makemigrations
-python manage.py migrate
+class Persona(models.Model):
+    nombre = models.TextField()
+    apellidos = models.TextField()
+    edad = models.TextField()
